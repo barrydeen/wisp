c83bb6ff3f240ce9cb31513ba39df018b110a157
Barry Deen <barrydeen@protonmail.com>
2026-02-28 11:38:38 -0500

    chore: bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wisp.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildTypes {
